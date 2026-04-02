0183750d6da34a40b7f63c445478810c5faaf77e
Bouchlaghmi <bouchlaghmikhalil@gmail.com>
2026-04-02 16:43:19 +0200

    dockerisation du backend Django

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "NAME": "careerquest",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "localhost",
+        "HOST": "db",
         "PORT": "5432",
     }
 }
